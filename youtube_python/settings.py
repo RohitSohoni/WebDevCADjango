e68ac9d04f72de8e55a3e0294e7513205c82a120
RohitSohoni <rohitsohoni155@gmail.com>
2022-07-19 01:11:34 +0100

    make it better

--- youtube_python/settings.py
+++ youtube_python/settings.py
@@ -1,7 +1,7 @@
 import os
-
-import django__heroku
+import django_heroku
 import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -112,4 +112,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-django__heroku.settings(locals())
+
+django_heroku.settings(locals())
